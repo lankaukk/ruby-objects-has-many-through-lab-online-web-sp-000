ad38c42769465800df2ae40795906626ccef2ce1
McKayla Lankau <lankaukk@yahoo.com>
2020-10-06 04:37:26 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,10 +16,6 @@ class Doctor
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
   
-  def appointments
-    Appointment.all.select {|appointment| appointment.doctor == self}
-  end
-  
   def new_appointment(date, patient)
     Appointment.new(date, patient, self)
   end 
